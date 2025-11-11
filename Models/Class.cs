f122aa704255a99dcba44c7f002a086858ea3932
Lars Jensen <laje63255@edu.ucl.dk>
2025-11-11 10:45:58 +0100

    Upload af code-eksempler

---
+++ Models/Class.cs
@@ -0,0 +1,13 @@
+﻿namespace HorseEvent.Models
+{
+    public class Class
+    {
+        public int ClassId { get; set; }
+        public int DisciplineId { get; set; }
+        public string ClassLevel { get; set; }
+        public decimal? Height { get; set; }
+        public string ProgramName { get; set; }
+        public DateTime ClassDate { get; set; }
+        public int? CompetitionId { get; set; }
+    }
+}
